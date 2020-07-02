51e9b4ad7045a0e7851c08b04b96dee8af0e0e0f
Nico Bellanich <nicolasbellanich@gmail.com>
2020-07-02 12:45:55 -0300

    get All reservas y vehiculos agregado

--- src/AWSSeerverlessAPI/Controllers/ReservasController.cs
+++ src/AWSSeerverlessAPI/Controllers/ReservasController.cs
@@ -1,126 +1,53 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-// using AWSSeerverlessAPI.Models;
-// using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AWSSeerverlessAPI.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using AWSSeerverlessAPI.Authorization;
+using Tiny.RestClient;
+using System.Net.Http;
 
+namespace AWSSeerverlessAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReservasController : ControllerBase
+    {
 
-// namespace AWSSeerverlessAPI.Controllers
-// {
-//     [Route("api/[controller]")]
-//     public class ReservasController : ControllerBase
-//     {
+        private readonly ILogger<ReservasController> _logger;
+        private readonly IConfiguration _config;
+        public ReservasController(ILogger<ReservasController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
 
-//         private static List<Reserva> listaReservas;
-//         private readonly ILogger<ReservasController> _logger;
+        [HttpGet]
+        public async Task<IEnumerable<Reserva>> Get()
+        {
+            try
+            {
+                _logger.LogInformation("Inicio");
 
-//         public ReservasController(ILogger<ReservasController> logger)
-//         {
-//             _logger = logger;
+                var client = new TinyRestClient(new HttpClient(), _config["url_api_proveedor"]);
 
-//             if (listaReservas == null)
-//             {
-//                 listaReservas = new List<Reserva>();
-//                 listaReservas.Add(new Reserva()
-//                 {
-//                     Id = "1",
-//                     FechaInicio = "2020-07-03",
-//                     FechaFin = "2020-07-04",
-//                     PrecioAlquiler = 100,
-//                     Activo = true,
-//                     IdVehiculo = "1"
-//                 });
-//                  listaReservas.Add(new Reserva()
-//                 {
-//                     Id = "2",
-//                     FechaInicio = "2020-07-27",
-//                     FechaFin = "2020-07-30",
-//                     PrecioAlquiler = 500,
-//                     Activo = false,
-//                     IdVehiculo = "1"
-//                 });
+                var reservas = await client.
 
-//             }
+                                GetRequest("Reservas").
+                                AddQueryParameter("name", "valor").
+                                WithOAuthBearer(await AuthorizationHelper.ObtenerAccessToken()).
+                                ExecuteAsync<List<Reserva>>();
 
-//         }
-//         // GET api/values/5
-//         [HttpGet("queryreservas/{idciudad}/{fechadesde}/{fechahasta}")]
-//         public List<Vehiculo> Get(string idciudad, string fechadesde, string fechahasta)
-//         {
+                return reservas;
 
-//             List<Vehiculo> listaVehiculos = new List<Vehiculo>();
-//             Controllers.VehiculosController controladorVehiculos = new Controllers.VehiculosController();
-//             listaVehiculos = controladorVehiculos.getListaVehiculosPorCiudad(idciudad);
-//             List<Vehiculo> listaReturn = new List<Vehiculo>();
-//             bool bandera = new bool();
-//             foreach (Vehiculo v in listaVehiculos)
-//             {
-//                 bandera = true;
-//                 foreach (Reserva r in listaReservas)
-//                 {
-//                     if (r.IdVehiculo == v.Id)
-//                     {
-//                         if (DateTime.Parse(r.FechaFin) < DateTime.Parse(fechadesde) || DateTime.Parse(r.FechaInicio) > DateTime.Parse(fechahasta))
-//                         {
-
-//                         }
-//                         else
-//                         {
-//                             bandera = false;
-//                         }
-//                     }
-//                 }
-//                 if (bandera)
-//                 {
-//                     listaReturn.Add(v);
-//                 }
-//             }
-
-//             return listaReturn;
-//         }
-
-//         // GET api/values
-//         [HttpGet]
-//         public IEnumerable<Reserva> Get()
-//         {
-//             return listaReservas;
-//         }
-
-//         // GET api/values/5
-//         [HttpGet("{id}")]
-//         public Reserva Get(string id)
-//         {
-//             return listaReservas.Find(reserva => reserva.Id == id);
-//         }
-
-//         // POST api/values
-//         [HttpPost]
-//         public Reserva Post([FromBody] Reserva bodyParam)
-//         {
-//             listaReservas.Add(bodyParam);
-//             return bodyParam;
-//         }
-
-//         // PUT api/values/5
-//         [HttpPut("{id}")]
-//         public int Put(string id, [FromBody] Reserva bodyParam)
-//         {
-//             Reserva reserva_encontrada = listaReservas.Find(reseva => reseva.Id == id);
-//             int index = listaReservas.IndexOf(reserva_encontrada);
-//             listaReservas[index].Activo = bodyParam.Activo;
-//             return index;
-//         }
-
-//         // DELETE api/values/5
-//         [HttpDelete("{id}")]
-//         public IEnumerable<Reserva> Delete(string id)
-//         {
-//             Reserva reseva_encontrada = listaReservas.Find(reserva => reserva.Id == id);
-//             int index = listaReservas.IndexOf(reseva_encontrada);
-//             listaReservas.RemoveAt(index);
-//             return listaReservas;
-//         }
-//     }
-// }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
