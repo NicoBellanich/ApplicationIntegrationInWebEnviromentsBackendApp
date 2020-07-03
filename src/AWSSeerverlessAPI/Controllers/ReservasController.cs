ec435d9048a8e07bb91ad3875b8bee10311e606b
Nico Bellanich <nicolasbellanich@gmail.com>
2020-07-03 16:02:26 -0300

    comit hoy

--- src/AWSSeerverlessAPI/Controllers/ReservasController.cs
+++ src/AWSSeerverlessAPI/Controllers/ReservasController.cs
@@ -9,6 +9,18 @@ using Microsoft.Extensions.Configuration;
 using AWSSeerverlessAPI.Authorization;
 using Tiny.RestClient;
 using System.Net.Http;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq.Expressions;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Linq;
 
 namespace AWSSeerverlessAPI.Controllers
 {
@@ -18,14 +30,17 @@ namespace AWSSeerverlessAPI.Controllers
 
         private readonly ILogger<ReservasController> _logger;
         private readonly IConfiguration _config;
+        private readonly DynamoDBContext _context;
         public ReservasController(ILogger<ReservasController> logger, IConfiguration config)
         {
             _logger = logger;
             _config = config;
+            var clienteAWS = new AmazonDynamoDBClient(RegionEndpoint.USEast1);
+            _context = new DynamoDBContext(clienteAWS);
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Reserva>> Get()
+        public async Task<IEnumerable<ReservasData>> Get()
         {
             try
             {
@@ -38,8 +53,12 @@ namespace AWSSeerverlessAPI.Controllers
                                 GetRequest("Reservas").
                                 WithOAuthBearer(await AuthorizationHelper.ObtenerAccessToken()).
                                 ExecuteAsync<List<Reserva>>();
+                
+                List<ScanCondition> condiciones = new List<ScanCondition>();
+                List<ReservasData> reservas_cliente = await _context.ScanAsync<ReservasData>(condiciones).GetRemainingAsync();
 
-                return reservas;
+
+                return reservas_cliente;
 
             }
             catch (Exception ex)
@@ -55,22 +74,49 @@ namespace AWSSeerverlessAPI.Controllers
             try
             {
                 _logger.LogInformation("Post Reserva");
+                Pasajero pasajero1 = new Pasajero()
+                {
+                    Nombre = "Juan",
+                    Apellido = "Perez",
+                    Dni = "12312312",
+                    Telefono = "351222522",
+                    Email = "j.p@juan.com"
+                };
+
+                ReservasResponse reserva1 = new ReservasResponse()
+                {
+                    idCotizacion = "b7538f04-aace-4d6d-a108-7e31bb165c57",
+                    pasajero = pasajero1
+                };
+
 
                 var client = new TinyRestClient(new HttpClient(), _config["url_api_proveedor"]);
 
-                
-                var data = new {IdCotizacion= reservasResponse.idCotizacion, reservasResponse=reservasResponse.pasajero};
-                
-                _logger.LogInformation("Empieza acá... Data:", data);
-                
+                _logger.LogInformation("Empieza acá... RESERVA1:", reserva1);
+                Console.WriteLine("RESERFVA:", reserva1);
+
+                var data = new { IdCotizacion = reserva1.idCotizacion, Pasajero = reserva1.pasajero };
+
+
+
                 var response = await client.
                                 PostRequest("Reservas", data).
                                 WithOAuthBearer(await AuthorizationHelper.ObtenerAccessToken()).
                                 ExecuteAsync<IActionResult>();
 
-                
+
+
+                ReservasData reservasData = new ReservasData()
+                {
+                    CodigoReserva = "123132",
+                    idCotizacion = "12331312",
+                    FechaHoraReserva = "2020-09-12T00:00:00",
+                    UsuarioReserva = "12223",
+                    TotalReserva = "8787"
+                };
+
+                await _context.SaveAsync<ReservasData>(reservasData);
                 return response;
- 
 
             }
             catch (Exception ex)
---
+++ src/AWSSeerverlessAPI/Models/ReservasData.cs
@@ -0,0 +1,21 @@
+
+namespace AWSSeerverlessAPI.Models
+{
+
+    public class ReservasData
+    {
+        public string CodigoReserva { get; set; }
+        public string idCotizacion { get; set; }
+        public string FechaHoraReserva { get; set; }
+        public string UsuarioReserva { get; set; }
+        public string TotalReserva { get; set; }
+
+        // public string toString(ReservasData reserva)
+        // {
+        //     return "{CodigoReserva:"reserva.CodigoReserva+}";
+        // }
+
+    }
+
+
+}
