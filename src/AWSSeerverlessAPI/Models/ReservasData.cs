262afe44e1e3e036e6463b655dcad73fd29eb496
Nico Bellanich <nicolasbellanich@gmail.com>
2020-07-03 18:03:52 -0300

    ola

--- src/AWSSeerverlessAPI/Models/ReservasData.cs
+++ src/AWSSeerverlessAPI/Models/ReservasData.cs
@@ -1,19 +1,20 @@
+using Amazon.DynamoDBv2.Model;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 
 namespace AWSSeerverlessAPI.Models
 {
 
+
     public class ReservasData
     {
+
         public string CodigoReserva { get; set; }
         public string idCotizacion { get; set; }
         public string FechaHoraReserva { get; set; }
         public string UsuarioReserva { get; set; }
         public string TotalReserva { get; set; }
-
-        // public string toString(ReservasData reserva)
-        // {
-        //     return "{CodigoReserva:"reserva.CodigoReserva+}";
-        // }
 
     }
 
